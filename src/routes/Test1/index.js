e344cd162b806e4f2fe07378a5e54b2910aa6541
angeline.budinata <angeline.budinata@BA-00023582.local>
2022-03-21 16:03:50 +0700

    Test 1

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 const Test1 = () => {
+  const [input, setInput] = useState();
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  };
+
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> {input}
       </p>
-      <input type="text" placeholder="input here"/>
+      <input type="text" placeholder="input here" onChange={handleChange} />
     </div>
   )
 }
