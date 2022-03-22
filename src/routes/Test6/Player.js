f508d2e44b7d7f4830f4b48b0f23aa063aeff6a4
angeline.budinata <angeline.budinata@BA-00023582.local>
2022-03-22 20:48:25 +0700

    Test 6 - part 1

--- src/routes/Test6/Player.js
+++ src/routes/Test6/Player.js
@@ -1,17 +1,39 @@
-import { cssVideo } from "./style";
+import { useState, useCallback } from "react";
+import { cssVideo, cssContainer } from "./style";
 
 const Player = () => {
+  const [videoRef, setVideoRef] = useState({});
+
+  const handlePlay = () => {
+    if(videoRef.paused) videoRef.play();
+    else videoRef.pause();
+  };
+
+  const handleMute = () => {
+    if(!videoRef.muted) videoRef.muted = true;
+    else videoRef.muted = false;
+  };
+
+  const callbackRef = useCallback(newRef => {
+    if(newRef) setVideoRef(newRef);
+  }, []);
+
   return(
-    <div>
-      <video
-        className={cssVideo}
-        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-      />
-      <button type="button">Play/Pause</button>
-      <button type="button">Mute/Unmute</button>
+    <>
+      <div className={cssContainer}>
+        <video
+          preload="metadata"
+          ref={callbackRef}
+          className={cssVideo}
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        />
+        
+      </div>
+      <button type="button" onClick={handlePlay}>Play/Pause</button>
+      <button type="button" onClick={handleMute}>Mute/Unmute</button>
       <input type="range"/>
-      <span>00:00/00:00</span>
-    </div>
+      <span>{videoRef.currentTime}/{videoRef.duration}</span>
+    </>
   )
 }
 
--- src/routes/Test6/style.js
+++ src/routes/Test6/style.js
@@ -1,9 +1,20 @@
 import { css } from 'react-emotion';
 
 export const cssVideo = css({
-  display: 'block',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
   width: "100%",
-  height: 600,
+  height: '100%',
   objectFit: 'cover',
   marginTop: 16,
+});
+
+export const cssContainer = css({
+  position: 'relative',
+  width: '100%',
+  overflow: 'hidden',
+  paddingTop: '75%',
 });
