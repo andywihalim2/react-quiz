e424d82e303c1379936c8600b99b17cd4cbb78df
angeline.budinata <angeline.budinata@BA-00023582.local>
2022-03-23 16:29:32 +0700

    Test 6 - part 2

--- src/routes/Test6/Player.js
+++ src/routes/Test6/Player.js
@@ -1,8 +1,16 @@
 import { useState, useCallback } from "react";
 import { cssVideo, cssContainer } from "./style";
 
+const convertDuration = duration => { // duration in seconds
+  const seconds = Math.floor(duration);
+
+  if(seconds < 60) return `0:${seconds > 10 ? seconds : `0${seconds}`}`;
+  return `${Math.floor(seconds/60)}:${seconds%60}`;
+};
+
 const Player = () => {
   const [videoRef, setVideoRef] = useState({});
+  const [currTime, setCurrTime] = useState(0);
 
   const handlePlay = () => {
     if(videoRef.paused) videoRef.play();
@@ -14,14 +22,24 @@ const Player = () => {
     else videoRef.muted = false;
   };
 
+  const handleRange = (e) => {
+    videoRef.currentTime = e.target.value;
+  };
+
   const callbackRef = useCallback(newRef => {
-    if(newRef) setVideoRef(newRef);
+    newRef.onloadedmetadata = function() {
+      setVideoRef(newRef);
+    };
+    newRef.ontimeupdate = function(e) {
+      setCurrTime(e.target.currentTime);
+    };
   }, []);
 
   return(
     <>
       <div className={cssContainer}>
         <video
+          controls
           preload="metadata"
           ref={callbackRef}
           className={cssVideo}
@@ -31,8 +49,8 @@ const Player = () => {
       </div>
       <button type="button" onClick={handlePlay}>Play/Pause</button>
       <button type="button" onClick={handleMute}>Mute/Unmute</button>
-      <input type="range"/>
-      <span>{videoRef.currentTime}/{videoRef.duration}</span>
+      <input type="range" min={0} max={videoRef.duration || 0} value={currTime} onChange={handleRange} />
+      <span>{convertDuration(currTime)}/{convertDuration(videoRef.duration)}</span>
     </>
   )
 }
